afbb5fdaad5058c50fcf1c49b4753816a7c90ae9
Dmitry <dimch2205@gmail.com>
2020-07-21 12:04:15 +0300

    Test API

--- static_src/App.jsx
+++ static_src/App.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './styles.style.css';
 import axios from 'axios';
 import { Root, CellButton, View, Panel, PanelHeader, Header, Group, Cell } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 import bridge from '@vkontakte/vk-bridge';
 
 export default class App extends React.Component {
   
  state = {
       activeView: 'header',
+      id:'7544278',
+      uri:'https://oauth.vk.com/blank.html',
+      response:' ',
+    };
+
+
+    componentDidMount(){
+      const {id,uri} = this.state;
+      axios.get(`https://oauth.vk.com/authorize?client_id={id}&display=popup&redirect_uri={uri}&scope=friends&response_type=token&v=5.120`)
+        .then((response) => { 
+          if (response.data.error) {
+              console.log(response.data.error.error_msg);
+
+          }else{
+            console.log(response.data);
+          }
+
+        }
+      );  
     }
 
   render() {
-    bridge.send("VKWebAppInit", {});
-    const result = fetch('https:\//api.vk.com/method/users.get?user_id=210700286&v=5.52' , {mode:'no-cors'});
-    setTimeout(()=>{
-      console.log(result);
-    },2000); 
+   bridge.send("VKWebAppInit", {});
+    
+    
+    
     return (
       <Root activeView={this.state.activeView}>
        <View id="header" activePanel="header" style={{border:"1px solid red"}}>
     <Panel id="header">
-    Имя Фамилия
+      Username
     </Panel>
   </View>
       </Root>
