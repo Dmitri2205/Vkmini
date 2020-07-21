54e53e8209ddccad46bb5d8d1ec52c7d5de46d27
Dmitry <dimch2205@gmail.com>
2020-07-21 21:33:39 +0300

    Request updation

--- static_src/App.jsx
+++ static_src/App.jsx
@@ -16,15 +16,15 @@ export default class App extends React.Component {
     componentDidMount(){
       const {id,uri} = this.state;
       bridge.send("VKWebAppInit", {});
-      window.location.href = 'https://oauth.vk.com/authorize?client_id=7544278&display=page&redirect_uri=https://dmitri2205.github.io/Vkmini/index.html&scope=friends&response_type=token&v=5.120';
-        //.then((response) => { 
-        //  if (response.data.error) {
-        //      console.log(response.data.error.error_msg);
-        //  }else{
-        //    console.log(response.data);
-        //  };
-        //  }
-        //);  
+     axios.get('https://oauth.vk.com/authorize?client_id=7545593&display=popup&redirect_uri=https://dmitri2205.github.io/Vkmini/&scope=friends&response_type=token&v=5.120')
+        .then((response) => { 
+         if (response.data.error) {
+             console.log(response.data.error.error_msg);
+         }else{
+           console.log(response.data);
+         };
+         }
+        );  
 
     }
 
