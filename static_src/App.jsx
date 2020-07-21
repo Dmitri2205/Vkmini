dfddad04916173413d1b39e06b19baacf66e57e0
Dmitry <dimch2205@gmail.com>
2020-07-22 00:24:53 +0300

    Auth test

--- static_src/App.jsx
+++ static_src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import axios from 'axios';
-import { Root, CellButton, View, Panel, PanelHeader, Header, Group, Cell } from '@vkontakte/vkui';
+import { Root, CellButton, View, Panel, PanelHeader, PanelHeaderBack, Header, Group, Cell } from '@vkontakte/vkui';
+import Icon28Notifications from '@vkontakte/icons/dist/28/notifications';
+import Icon28SettingsOutline from '@vkontakte/icons/dist/28/settings_outline';
 import '@vkontakte/vkui/dist/vkui.css';
 import bridge from '@vkontakte/vk-bridge';
 
@@ -11,22 +14,26 @@ export default class App extends React.Component {
       id:' ',
       uri:' ',
       response:' ',
+      reg:'/^access_token=$\d|[a-zA-z]^&$/',
     };
 
     componentDidMount(){
       const {id,uri} = this.state;
       bridge.send("VKWebAppInit", {});
-     axios.get('https://oauth.vk.com/authorize?client_id=7545593&display=popup&redirect_uri=https://dmitri2205.github.io/Vkmini/&scope=friends&response_type=token&v=5.120')
-        .then((response) => { 
-         if (response.data.error) {
-             console.log(response.data.error.error_msg);
-         }else{
-           console.log(response.data);
-         };
-         }
-        );  
-
+     // window.location = 'https:\//oauth.vk.com/authorize?client_id=7545593&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.120';  
     }
+
+
+authFunction = () => {
+	alert('Redirect');
+     window.location = 'https:\//oauth.vk.com/authorize?client_id=7545593&display=page&redirect_uri=https:\//oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.120';  
+     const re = new RegExp(this.state.reg);
+     var result = re.exec(window.location.href);
+     console.log(re);
+};
+
+
+
 
   render() {
     
@@ -34,9 +41,12 @@ export default class App extends React.Component {
     
     return (
       <Root activeView={this.state.activeView}>
-       <View id="header" activePanel="header" style={{border:"1px solid red"}}>
-    <Panel id="header">
-      Username
+      <View id="header" activePanel="header__name" style={{border:"1px solid red"}}>
+    <Panel id="header__name">
+     <PanelHeader left={<PanelHeaderBack onClick={this.props.onBackClick} />}>
+  		Назад {this.props.platform}
+	 </PanelHeader>
+    		<CellButton onClick={()=>{this.authFunction()}}>Auth</CellButton>
     </Panel>
   </View>
       </Root>
